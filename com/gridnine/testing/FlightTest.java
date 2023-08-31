b1c0f52dc00a99b992cb2c4dc96657f9550f8e9f
Hdale <EkaterinaR4@yandex.ru>
2023-08-31 19:06:09 +0300

    JUnit

---
+++ com/gridnine/testing/FlightTest.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+
+public class FlightTest {
+
+
+    public static void main(String[] args) {
+        Result results = JUnitCore.runClasses(FlightTest.class);
+        for (Failure failure : results.getFailures()) {
+            System.out.println("Тест не пройден: " + failure);
+        }
+    }
+
+
+    @Test
+    public void testNowFlightFilter() throws Exception {
+        System.out.println("testNowFlightFilter()");
+
+        List<Flight> flyList = FlightBuilder.createFlights();
+        Main m = new Main();
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Flight> lf = m.flyFilter(flyList, now);
+        lf.forEach(System.out::println);
+        Assert.assertEquals(0, lf.size());
+    }
+
+
+    @Test
+    public void testOldFlightFilter() throws Exception {
+        System.out.println("testOldFlightFilter()");
+
+        List<Flight> flyList = FlightBuilder.createFlights();
+        Main m = new Main();
+        LocalDateTime myNow = LocalDateTime.now().plusDays(3).plusHours(7);
+
+        List<Flight> lf = m.flyFilter(flyList, myNow);
+        lf.forEach(System.out::println);
+        Assert.assertEquals(4, lf.size());
+    }
+
+}
--- com/gridnine/testing/Main.java
+++ com/gridnine/testing/Main.java
@@ -1,14 +1,12 @@
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        List<Flight> flyList = FlightBuilder.createFlights();
-        final LocalDateTime myNow = LocalDateTime.now();        //TODO  .plusDays(3).plusHours(7);
-        flyList.parallelStream()
+    public List<Flight> flyFilter(List<Flight> flyList,  LocalDateTime myNow) {
+        return (flyList.parallelStream()
                 .filter(fl -> {
                     LocalDateTime local = null;
                     for (Segment sg : fl.getSegments()) {
@@ -27,6 +25,21 @@ public class Main {
                     }
                     return true;
                 })
-                .forEach(System.out::println);
+                .collect(Collectors.toList()));
+//                .forEach(System.out::println);
     }
+
+
+    public static void main(String[] args) {
+
+        List<Flight> flyList = FlightBuilder.createFlights();
+        Main m = new Main();
+        LocalDateTime now = LocalDateTime.now();
+//        LocalDateTime myNow = LocalDateTime.now().plusDays(3).plusHours(7);
+
+        m.flyFilter(flyList, now).forEach(System.out::println);
+    }
+
+
+
 }
